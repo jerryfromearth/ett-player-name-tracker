01e52636cb9fade6ae68a4458fa790155af323be
jerryfromearth <jerryfromearth@gmail.com>
2022-01-14 20:46:10 +0100

    switched from purely read/write json to read/write sqlite db

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 
 const fetch = require("node-fetch");
 const fs = require("fs").promises;
+const sqlite3 = require("sqlite3").verbose();
+const { open } = require("sqlite");
 
-let dbFile = "db.json";
+let dbFile = "users.db";
 const INTERVAL_MINUTES = 10;
-let mapUsers = new Map();
 
 function parseSnapshot(json) {
   let users = json.OnlineUses.map((user) => {
@@ -19,10 +20,36 @@ function parseSnapshot(json) {
   return users;
 }
 
-async function track() {
-  console.log(new Date().toISOString());
+async function loadDB() {
+  let users = [];
+  // Parse users from json
+  let db = await open({ filename: dbFile, driver: sqlite3.Database });
 
-  console.log("Poll start - currently tracked users:", mapUsers.size);
+  console.log("Connected to the database.");
+
+  try {
+    await db.exec(
+      "CREATE TABLE users(Id, UserName, Device, Platform, ELO, History)"
+    );
+  } catch (error) {}
+  return db;
+}
+
+async function exportDB(db) {
+  let data = await db.all("SELECT * FROM users");
+  data = data.map((row) => {
+    row.History = JSON.parse(row.History);
+    return row;
+  });
+  await fs.writeFile("users.json", JSON.stringify(data, null, 2), "utf-8");
+}
+
+async function track() {
+  let db = await loadDB();
+
+  console.log(new Date().toISOString());
+  let count = await db.all("SELECT COUNT(*) as count FROM users");
+  console.log("Poll start - Currently tracked users:", count[0]);
 
   // Fetch new_users
   let onlineUsers = [];
@@ -41,57 +68,66 @@ async function track() {
   console.log("Online users:", onlineUsers.length);
 
   // Merge new_users into users
-  onlineUsers.forEach((newUser) => {
-    let existingUser = mapUsers.get(newUser.Id);
-    if (existingUser === undefined) {
-      mapUsers.set(newUser.Id, newUser);
-      console.log(`Adding new user: ${newUser.UserName}`);
-    } else {
-      if (newUser.UserName !== existingUser.UserName) {
-        existingUser.UserName = newUser.UserName;
-        existingUser.History.push(newUser.History[0]);
-        mapUsers.set(existingUser.Id, existingUser);
-        console.log(
-          `Found new username!\n${JSON.stringify(existingUser, null, 2)}`
-        );
-      }
+  for (let onlineUser of onlineUsers) {
+    let user = await db.get(`SELECT * FROM users WHERE Id=?`, onlineUser.Id);
+
+    if (user == null) {
+      // User doesn't exist in DB. Insert.
+      const result = await db.run(
+        "INSERT INTO users (Id,UserName,Device,Platform,ELO,History) VALUES (?,?,?,?,?,?)",
+        onlineUser.Id,
+        onlineUser.UserName,
+        onlineUser.Device,
+        onlineUser.Platform,
+        onlineUser.ELO,
+        JSON.stringify(onlineUser.History, null, 2)
+      );
+      continue;
     }
-  });
 
-  console.log("Poll end - Currently tracked users:", mapUsers.size);
+    // user already exist in db
+    let historyBak = user.History;
+    user.History = JSON.parse(user.History);
+    if (
+      user.History.filter((entry) => entry.UserName === onlineUser.UserName)
+        .length === 0
+    ) {
+      console.log(
+        `Found new username! old: ${JSON.stringify(
+          user,
+          null,
+          2
+        )}, new: ${JSON.stringify(onlineUser, null, 2)}`
+      );
+      user.History = user.History.concat(onlineUser.History);
+      console.log(`Merged: user: ${JSON.stringify(user, null, 2)}`);
+    }
 
-  let users = [];
-  for (const [Id, user] of mapUsers) {
-    users.push(mapUsers.get(Id));
+    const result = await db.run(
+      "UPDATE users SET UserName=(?),Device=(?),Platform=(?),ELO=(?),History=(?) WHERE ID=(?)",
+      onlineUser.UserName,
+      onlineUser.Device,
+      onlineUser.Platform,
+      onlineUser.ELO,
+      JSON.stringify(user.History, null, 2),
+      onlineUser.Id
+    );
   }
 
-  users = users.sort((user1, user2) => {
-    return user2.History.length - user1.History.length;
-  });
-  console.log(
-    `Current champion:\n${
-      users.length == 0 ? "" : JSON.stringify(users[0], null, 2)
-    }`
-  );
+  count = await db.all("SELECT COUNT(*) as count FROM users");
+  console.log("Poll end - Currently tracked users:", count[0]);
 
-  await fs.writeFile(dbFile, JSON.stringify(users, null, 2), "utf8");
+  await exportDB(db);
+
+  await db.close();
+
+  //await fs.writeFile(dbFile, JSON.stringify(users, null, 2), "utf8");
   console.log("");
 }
 
 async function main() {
-  // Parse users from json
-  try {
-    let fileContent = await fs.readFile(dbFile, "utf8");
-    let users = JSON.parse(fileContent);
-    for (const user of users) {
-      mapUsers.set(user.Id, user);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-
   track();
-  setInterval(track, 1000 * 60 * INTERVAL_MINUTES);
+  setInterval(await track, 1000 * 60 * INTERVAL_MINUTES);
 }
 
 main();
